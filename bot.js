ca8c39a10ab38fea40091fc5380e549943042ea6
Süleyman BAL <32075759+tuleymanbal@users.noreply.github.com>
2020-12-01 05:45:32 +0300

    tB

---
+++ bot.js
@@ -0,0 +1,153 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const fs = require("fs");
+const db = require("quick.db");
+require("./util/eventLoader")(client);
+
+const log = message => {
+  console.log(`${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on("warn", e => {
+  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+});
+
+client.on("error", e => {
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+});
+
+   //........MESAJ ISTATISTIK........//
+
+client.on("message", async (message, args) => {
+  if (message.channel.id == 'kanal id') return; // saymasını istemediğiniz yazı kanalı idsi
+  if (message.author.bot === false) {
+    await db.add(`puan_${message.guild.id}_${message.author.id}`, 1);
+    await db.add(`puanc_${message.guild.id}_${message.channel.id}`, 1);
+    await db.add(`puanuc_${message.author.id}_${message.channel.id}`, 1);
+  }
+});
+
+//........SES ISTATISTIK........//
+
+client.on("voiceStateUpdate", async (oldMember, newMember) => {
+	if (oldMember.channel.id === 'kanal id') return; // saymasını istemediğiniz ses kanalı idsi
+  if (!oldMember.user.bot) {
+    let oldChannel = oldMember.voiceChannel;
+
+    let newChannel = newMember.voiceChannel;
+
+    if (oldChannel === undefined && newChannel !== undefined) {
+      db.set(`girisses.${oldMember.user.id}.${oldMember.guild.id}`, Date.now());
+    } else if (newChannel === undefined) {
+      let ilksessüre = await db.fetch(
+        `girisses.${oldMember.user.id}.${oldMember.guild.id}`
+      );
+      let time = Date.now() - ilksessüre;
+      await db.add(
+        "voicei_" + oldMember.guild.id + "_" + oldMember.user.id,
+        time
+      );
+
+      await db.add(
+        "voicec_" + oldMember.guild.id + "_" + oldMember.voiceChannelID,
+        time
+      );
+
+      await db.add(
+        "voiceuc_" + oldMember.user.id + "_" + oldMember.voiceChannelID,
+
+        time
+      );
+    }
+  }
+});
+
+  client.on("ready", async () => {
+    client.user.setActivity('discord.gg/CodeArius')
+    client.user.setStatus("dnd");
+  })
+client.login(ayarlar.token);
